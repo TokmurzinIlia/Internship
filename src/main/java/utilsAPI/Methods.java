ea4a88f882076df6f5cd65ac61a56a5af0255eca
TokmurzinIlia <ilia.tokmurzin@gmail.com>
2022-03-09 14:30:42 +0300

    add test change register letter commit

--- src/main/java/utilsAPI/Methods.java
+++ src/main/java/utilsAPI/Methods.java
@@ -3,6 +3,8 @@ package utilsAPI;
 import io.qameta.allure.Step;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -19,10 +21,33 @@ public class Methods {
                 .when().get(CODES+countryCode);
     }
 
+    @Step("Status code validate 200")
+    public static ValidatableResponse statusCodeValidate200(Response response){
+       return response
+               .then()
+               .statusCode(200);
+    }
+
     @Step("Getting a list borders from response by value {0} and {1}")
     public static List<String> getListBoardersFromResponse(Response response, String key){
        return response
                .then().extract().jsonPath().get(key);
     }
+
+    @Step("Convert a string to a string whose first letter is capitalized followed by lowercase")
+    public static String сonvertStringToStringWhoseFirstLetterCapitalized(String str) {
+        return
+                StringUtils.capitalize(str.toLowerCase());
+    }
+
+    @Step("Convert a string to a string whose second letter is capitalized followed by lowercase")
+    public static String сonvertStringToStringWhoseDesiredLetterCapitalized(String str, int desiredChar) {
+
+        StringBuilder builder = new StringBuilder(str.toLowerCase());
+        builder.setCharAt(desiredChar, Character.toUpperCase(str.charAt(desiredChar)));
+        return builder.toString();
+
+    }
+
 
 }
--- src/test/java/tests/RestTests.java
+++ src/test/java/tests/RestTests.java
@@ -2,41 +2,117 @@ package tests;
 
 import dataproviders.DataProviderCountryBorders;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.opentest4j.AssertionFailedError;
 import utilsAPI.Log;
 import utilsAPI.Methods;
 import java.util.List;
 
+
 import static org.junit.jupiter.api.Assertions.*;
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 public class RestTests {
 
-    @Order(1)
-    @ParameterizedTest(name = "Test status code {0}")
+
+    @ParameterizedTest(name = "Test status code with valid country code to uppercase {0}")
     @ArgumentsSource(DataProviderCountryBorders.class)
-    public void statusCodeTest(String country){
+    public void statusCodeTestWithValidData(String countryCode){
+
         Response rs = Methods
-                .getCountryBoarderResponse(country);
+                .getCountryBoarderResponse(countryCode);
 
         Log.info("Request response received");
 
-        rs.then().statusCode(200);
+        Methods.statusCodeValidate200(rs);
 
         Log.info("Status response code 200");
 
         System.out.println();
     }
 
-    @Order(2)
+    @ParameterizedTest(name = "Test status code with valid country code to lowercase {0}")
+    @ArgumentsSource(DataProviderCountryBorders.class)
+    public void statusCodeTestToLowerCaseWithValidData(String countryCode){
+
+        Response rs = Methods
+                .getCountryBoarderResponse(countryCode.toLowerCase());
+
+        Log.info("Request response received");
+
+        Methods.statusCodeValidate200(rs);
+
+        Log.info("Status response code 200");
+
+        System.out.println();
+    }
+
+    @ParameterizedTest(name = "Test status code with valid country code {0} first letter capital followed by lowercase")
+    @ArgumentsSource(DataProviderCountryBorders.class)
+    public void statusCodeTestFirstLetterCapitalizedWithValidData(String countryCode){
+
+        countryCode = Methods.сonvertStringToStringWhoseFirstLetterCapitalized(countryCode);
+
+        Log.info("Convert a country code to a string whose first letter is capitalized followed by lowercase");
+
+        Response rs = Methods
+                .getCountryBoarderResponse(countryCode);
+
+        Log.info("Request response received");
+
+        Methods.statusCodeValidate200(rs);
+
+        Log.info("Status response code 200");
+
+        System.out.println();
+    }
+
+    @ParameterizedTest(name = "Test status code with valid country code {0} second letter capital followed by lowercase")
+    @ArgumentsSource(DataProviderCountryBorders.class)
+    public void statusCodeTestSecondLetterCapitalizedWithValidData(String countryCode){
+
+        countryCode = Methods.сonvertStringToStringWhoseDesiredLetterCapitalized(countryCode, 1);
+
+        Log.info("Convert a country code to a string whose second letter is capitalized followed by lowercase");
+
+        Response rs = Methods
+                .getCountryBoarderResponse(countryCode);
+
+        Log.info("Request response received");
+
+        Methods.statusCodeValidate200(rs);
+
+        Log.info("Status response code 200");
+
+        System.out.println();
+    }
+
+    @ParameterizedTest(name = "Test status code with valid country code {0} second letter capital followed by lowercase")
+    @ArgumentsSource(DataProviderCountryBorders.class)
+    public void statusCodeTestThirdLetterCapitalizedWithValidData(String countryCode){
+
+        countryCode = Methods.сonvertStringToStringWhoseDesiredLetterCapitalized(countryCode, 2);
+
+        Log.info("Convert a country code to a string whose second letter is capitalized followed by lowercase");
+
+        Response rs = Methods
+                .getCountryBoarderResponse(countryCode);
+
+        Log.info("Request response received");
+
+        Methods.statusCodeValidate200(rs);
+
+        Log.info("Status response code 200");
+
+        System.out.println();
+    }
+
+
     @ParameterizedTest(name = "Test that checks a list of boarders by values country code {0}")
     @ArgumentsSource(DataProviderCountryBorders.class)
     public void bordersTest(String countryCode, List<String> expectedListCountryBorders, String key){
+
         Response rs = Methods
                 .getCountryBoarderResponse(countryCode);
 
@@ -76,7 +152,7 @@ public class RestTests {
 //        System.out.println();
 //    }
 
-    @Order(3)
+
     @ParameterizedTest(name = "A test that checks the reciprocity of the value of the border of a given country {0} " +
             "with the current list of bordering countries ")
     @ArgumentsSource(DataProviderCountryBorders.class)
