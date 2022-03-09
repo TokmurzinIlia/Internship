711a6858e86462eff4bb1ee07c030450f01af5c8
TokmurzinIlia <ilia.tokmurzin@gmail.com>
2022-03-09 17:13:47 +0300

    add test null commit

--- src/test/java/tests/RestTests.java
+++ src/test/java/tests/RestTests.java
@@ -3,6 +3,8 @@ package tests;
 import dataproviders.DataProviderCountryBorders;
 import io.restassured.response.Response;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -57,7 +59,7 @@ public class RestTests {
 
     @ParameterizedTest(name = "Test status code with valid country code to lowercase {0}")
     @ArgumentsSource(DataProviderCountryBorders.class)
-    public void statusCodeTestToLowerCaseWithValidData(String countryCode){
+    public void statusCodeTestToLowerCaseWithValidDataCountryCode(String countryCode){
 
         Response rs = Methods
                 .getCountryBoarderResponse(countryCode.toLowerCase());
@@ -73,7 +75,7 @@ public class RestTests {
 
     @ParameterizedTest(name = "Test status code with valid country code {0} first letter capital followed by lowercase")
     @ArgumentsSource(DataProviderCountryBorders.class)
-    public void statusCodeTestFirstLetterCapitalizedWithValidData(String countryCode){
+    public void statusCodeTestFirstLetterCapitalizedWithValidDataCountryCode(String countryCode){
 
         countryCode = Methods.сonvertStringToStringWhoseFirstLetterCapitalized(countryCode);
 
@@ -93,7 +95,7 @@ public class RestTests {
 
     @ParameterizedTest(name = "Test status code with valid country code {0} second letter capital followed by lowercase")
     @ArgumentsSource(DataProviderCountryBorders.class)
-    public void statusCodeTestSecondLetterCapitalizedWithValidData(String countryCode){
+    public void statusCodeTestSecondLetterCapitalizedWithValidDataCountryCode(String countryCode){
 
         countryCode = Methods.сonvertStringToStringWhoseDesiredLetterCapitalized(countryCode, 1);
 
@@ -113,7 +115,7 @@ public class RestTests {
 
     @ParameterizedTest(name = "Test status code with valid country code {0} second letter capital followed by lowercase")
     @ArgumentsSource(DataProviderCountryBorders.class)
-    public void statusCodeTestThirdLetterCapitalizedWithValidData(String countryCode){
+    public void statusCodeTestThirdLetterCapitalizedWithValidDataCountryCode(String countryCode){
 
         countryCode = Methods.сonvertStringToStringWhoseDesiredLetterCapitalized(countryCode, 2);
 
@@ -134,10 +136,26 @@ public class RestTests {
     @ParameterizedTest(name = "Test status code with invalid data {0}")
     @CsvSource({"a", "z", "g", "1", "50", "99", "100", "101", "199", "200", "999", "1000",
             "1001", "1999", "12345678954621", "0", "-1", "-50", "-99", "-100", "-101", "-12345678954621", "russia", "<script>"})
-    public void statusCodeTestWithInValidData(String countryCode){
+    public void statusCodeTestWithInValidDataCountryCode(String countryCode){
 
         Response rs = Methods
                 .getCountryBoarderResponse(countryCode);
+
+        Log.info("Request response received");
+
+        Methods.statusCodeValidate400(rs);
+
+        Log.info("Status response code 400");
+
+        System.out.println();
+    }
+
+    @Test
+    @DisplayName("Test status code with country code null")
+    public void statusCodeTestWithNullCountryCode(){
+
+        Response rs = Methods
+                .getCountryBoarderResponse(null);
 
         Log.info("Request response received");
 
